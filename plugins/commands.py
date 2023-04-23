e75ac9865d90752cc878dfc2c4cca00203dc40f3
Sanju1t85 <101995551+Sanju1t85@users.noreply.github.com>
2023-04-23 19:16:45 +0530

    4

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,7 +23,7 @@ async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [
             [
-                InlineKeyboardButton('⚡ UᎮDΛTΞS ⚡', url='https://t.me/sksbackup1')
+                InlineKeyboardButton('⚡ Main Channel ⚡', url='https://t.me/sksbackup1')
             ],
             [
                 InlineKeyboardButton('⚡ SUBSCᏒIBΞ ⚡', url=f"https://youtube.com/@telegramwallah3835"),
@@ -47,8 +47,11 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
+            InlineKeyboardButton('⚡ GROUP 1 ⚡', url='https://t.me/+j42EsbeJsHMwMzQ9'),
+            InlineKeyboardButton('🌟 GROUP 2 🌟', url='https://t.me/+kXJVKIsnmxUxYWE1')
+            ],[
             InlineKeyboardButton('⚡ SUBSCᏒIBΞ ⚡', url='https://youtube.com/@telegramwallah3835'),
-            InlineKeyboardButton('🤖 UᎮDΛTΞS 🤖', url='https://t.me/sksbackup1')
+            InlineKeyboardButton('🤖 Main Channel 🤖', url='https://t.me/sksbackup1')
             ],[
             InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
             InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
@@ -93,8 +96,11 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
+            InlineKeyboardButton('⚡ GROUP 1 ⚡', url='https://t.me/+j42EsbeJsHMwMzQ9'),
+            InlineKeyboardButton('🌟 GROUP 2 🌟', url='https://t.me/+kXJVKIsnmxUxYWE1')
+            ],[
             InlineKeyboardButton('⚡ SUBSCᏒIBΞ ⚡', url='https://youtube.com/@telegramwallah3835'),
-            InlineKeyboardButton('🤖 UᎮDΛTΞS 🤖', url='https://t.me/sksbackup1')
+            InlineKeyboardButton('🤖 Main Channel 🤖', url='https://t.me/sksbackup1')
             ],[
             InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
             InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
@@ -247,7 +253,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://youtube.com/@GreyMattersYT') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://youtube.com/@telegramwallah3835') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
