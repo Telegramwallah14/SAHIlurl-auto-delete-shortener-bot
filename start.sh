fd196146eb759e4b7d1ef85015150702e2627096
Sahidmalik001 <106477157+Sahidmalik001@users.noreply.github.com>
2023-04-23 22:42:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Sanju1t85/url-auto-delete-shortener-bot-1 /Eva
+  git clone https://github.com/Sanju1t85/url-auto-delete-shortener-bot-1 /url-auto-delete-shortener-bot-1
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Eva
+  git clone $UPSTREAM_REPO /url-auto-delete-shortener-bot-1
 fi
-cd /Eva
+cd /url-auto-delete-shortener-bot-1
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
